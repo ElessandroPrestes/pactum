53476aa6f6558a56149b4eb0ddee682831b86ae7
elessandro <elessandrodev@gmail.com>
2026-05-14 18:34:40 -0300

    feat(provider): cria repository service provider vazio pronto para bindings

---
+++ app/Providers/RepositoryServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+}
