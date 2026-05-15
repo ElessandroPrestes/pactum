638a9e63263c8ca76778adbf57d0e070e51bbf9b
elessandro <elessandrodev@gmail.com>
2026-05-15 09:02:02 -0300

    feat(policy): adiciona policies de client service e contract

---
+++ app/Policies/ServicePolicy.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Policies;
+
+use App\Models\Service;
+use App\Models\User;
+
+class ServicePolicy
+{
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    public function view(User $user, Service $service): bool
+    {
+        return true;
+    }
+
+    public function create(User $user): bool
+    {
+        return true;
+    }
+
+    public function update(User $user, Service $service): bool
+    {
+        return true;
+    }
+
+    public function delete(User $user, Service $service): bool
+    {
+        return true;
+    }
+}
