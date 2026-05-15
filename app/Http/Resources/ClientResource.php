72f480a9f06664f9ac03e9b12c6a9cea366beefc
elessandro <elessandrodev@gmail.com>
2026-05-14 22:21:18 -0300

    feat(resource): adiciona client resource

---
+++ app/Http/Resources/ClientResource.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Resources;
+
+use App\Models\Client;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/**
+ * @mixin Client
+ */
+class ClientResource extends JsonResource
+{
+    /**
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'nome' => $this->nome,
+            'documento' => $this->documento,
+            'tipo_documento' => $this->tipo_documento->value,
+            'email' => $this->email,
+            'status' => $this->status->value,
+            'created_at' => $this->created_at?->toIso8601String(),
+            'updated_at' => $this->updated_at?->toIso8601String(),
+        ];
+    }
+}
